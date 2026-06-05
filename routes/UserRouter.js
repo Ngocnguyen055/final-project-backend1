9c290780370476c093604cda8a296058a528fc66
Ngocnguyen055 <nguyenchingoc9@gmail.com>
2026-06-05 22:20:49 +0700

    h

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 const User = require("../db/userModel.js");
 const Photo = require("../db/photoModel.js");
 
